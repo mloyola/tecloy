49586b2cea5535df34755a1a912699a2515fc479
Mauricio Loyola <mloyola@mbcomunicacionessac.com>
2014-07-21 20:29:18 -0500

    Create form empresa and contacto

--- src/Tecloy/FrontendBundle/Controller/DefaultController.php
+++ src/Tecloy/FrontendBundle/Controller/DefaultController.php
@@ -4,10 +4,68 @@ namespace Tecloy\FrontendBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('FrontendBundle:Default:index.html.twig');
     }
+
+    public function empresaAction()
+    {
+        return $this->render('FrontendBundle:Default:empresa.html.twig');
+    }
+
+    public function contactoAction(Request $request)
+    {    	
+    	$request = $this->getRequest();                     
+
+        $form = $this->createFormBuilder()     
+        	->add('name', 'text',array('max_length' => '50','attr' => array('class' => 'form-control','placeholder' => '* Escriba su nombre completo', 'data-rule' => 'maxlen:4', 'data-msg' => 'Introduzca por lo menos 4 caracteres')))                
+        	->add('email', 'text',array('max_length' => '100','attr' => array('class' => 'form-control','placeholder' => '* Ingrese su dirección de correo electrónico', 'data-rule' => 'maxlen:4', 'data-msg' => 'Por favor, introduzca un email válido')))                              
+            ->add('subject', 'text',array('max_length' => '50','attr' => array('class' => 'form-control','placeholder' => '* Ingresa el asunto', 'data-rule' => 'maxlen:4', 'data-msg' => 'Introduzca por lo menos 4 caracteres')))
+            ->add('message', 'textarea',array('max_length' => '300','attr' => array('class' => 'form-control','placeholder' => '* Tu mensaje aquí', 'data-rule' => 'maxlen:4', 'data-msg' => 'Por favor, escribir algo', 'rows' => '10')))                                        
+            ->getForm();        
+
+        if ($request->getMethod() == 'POST')
+            {                
+                $form->handleRequest($request);                
+                if ($form->isValid())
+                {                	
+                    $datos = $form->getData();                       
+
+                    $contenido = sprintf(" Remitente: %s \n\n E-mail: %s \n\n Asunto: %s \n\n Mensaje: %s \n\n Navegador: %s \n Dirección IP: %s \n",
+                        $datos['name'],
+                        $datos['email'],
+                        $datos['subject'],
+                        htmlspecialchars($datos['message']),
+                        $peticion->server->get('HTTP_USER_AGENT'),
+                        $peticion->server->get('REMOTE_ADDR')
+                    );
+
+                    $mensaje = \Swift_Message::newInstance()
+                        ->setSubject('Contacto')
+                        ->setFrom($datos['email'])
+                        ->setTo('info@tecloy.com')
+                        ->setBody($contenido)
+                    ;
+
+                    $this->container->get('mailer')->send($mensaje);
+
+                    $this->get('session')->getFlashBag()->add('info','Tu mensaje ha sido enviado. Gracias');                                                                                                         
+                }                    
+                return $this->redirect($this->generateUrl('frontend_contacto'));                      
+            }
+
+            return $this->render('FrontendBundle:Default:contacto.html.twig', array('form' => $form->createView()));
+        
+    }
+   
 }
